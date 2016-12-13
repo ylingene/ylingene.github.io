d1ab1fbe7c0558ffc7a8af0099ad7e3c1e848f64
ylingene <ylingene@gmail.com>
2016-12-13 01:20:08 -0500

    cleaned up js and testing better image loading

--- js/photography_script.js
+++ js/photography_script.js
@@ -1,3 +1,74 @@
+/* Isotope fitrows and filter */
+var $grid = $('.grid').isotope({
+  layoutMode: 'fitRows',
+  itemSelector: '.grid-item',
+  fitRows: {
+    gutter: 10
+  }
+});
+
+if($(window).width() <= 768) {
+  $grid.isotope({
+    layoutMode: 'fitRows',
+    itemSelector: '.grid-item',
+    fitRows: {
+      gutter: 0
+    }
+  });
+}
+
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
+});
+
+// filter functions
+var filterFns = {}
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+$('#filters button').click(function(){
+var selector = $(this).attr('data-filter');
+
+  // add photos of filter type to data-fancybox-group (slideshow only shows photos selected by the filter type)
+  if(selector == "*") {
+    $(".fancybox").attr("data-fancybox-group", "gallery");
+  } 
+  else { 
+    $(selector).find(".fancybox").attr("data-fancybox-group", selector);
+  }
+
+  // filter thumbnails based on selector (filter)
+  $grid.isotope({ filter: selector });
+
+  $('#filters > button').removeClass('is-checked'); 
+  $(this).addClass('is-checked'); 
+  return false;
+});
+
+/* Fancybox initiation */
+$(".fancybox").fancybox();
+$(".fancybox").attr('rel', 'gallery').fancybox({
+    beforeShow: function () {
+        /* Disable right click */
+        $.fancybox.wrap.bind("contextmenu", function (e) {
+          return false; 
+        });
+    }
+});
+
+
 $(document).ready( function() {
 
   // show nav bar at a certain breakpoint depending on the window size (corresponds to the hero-div size)
@@ -24,67 +95,21 @@ $(document).ready( function() {
       }
   });
 
-  /* Fancybox initiation */
-  $(".fancybox").fancybox();
-  $(".fancybox").attr('rel', 'gallery').fancybox({
-      beforeShow: function () {
-          /* Disable right click */
-          $.fancybox.wrap.bind("contextmenu", function (e) {
-            return false; 
-          });
-      }
-  });
 
-  /* Isotope fitrows and filter */
-
-  $('.grid').isotope({
-  	layoutMode: 'fitRows',
-    itemSelector: '.grid-item',
-    fitRows: {
-      gutter: 10
-    }
-  });
-
-  if($(window).width() <= 768) {
-    $('.grid').isotope({
-    layoutMode: 'fitRows',
-    itemSelector: '.grid-item',
-    fitRows: {
-      gutter: 0
-    }
-  });
-  }
-
-var $grid = $('.grid').imagesLoaded( function() {
-  // init Isotope after all images have loaded
-  $grid.isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-  });
-});
+  // $grid = $('.grid').imagesLoaded( function() {
+  //   // init Isotope after all images have loaded
+  //   $grid.isotope({
+  //     itemSelector: '.element-item',
+  //     layoutMode: 'fitRows'
+  //   });
+  // });
 
 // init Isotope
   // var $grid = $('.grid').isotope({
   //   itemSelector: '.element-item',
   //   layoutMode: 'fitRows'
   // });
-  // filter functions
-  var filterFns = {}
-  // bind filter button click
-  $('.filters-button-group').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
-    $grid.isotope({ filter: filterValue });
-  });
-  // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
-    });
-  });
+  
   
   // $('.grid').imagesLoaded( function(){
   //         $('.grid').isotope({
@@ -107,24 +132,7 @@ var $grid = $('.grid').imagesLoaded( function() {
   // $('.fadein').fadeIn(1000);
 
 
-  $('#filters button').click(function(){
-  var selector = $(this).attr('data-filter');
-
-    // add photos of filter type to data-fancybox-group (slideshow only shows photos selected by the filter type)
-    if(selector == "*") {
-      $(".fancybox").attr("data-fancybox-group", "gallery");
-    } 
-    else { 
-      $(selector).find(".fancybox").attr("data-fancybox-group", selector);
-    }
-
-    // filter thumbnails based on selector (filter)
-    $grid.isotope({ filter: selector });
-
-    $('#filters > button').removeClass('is-checked'); 
-    $(this).addClass('is-checked'); 
-    return false;
-  });
+  
 
 });
 
