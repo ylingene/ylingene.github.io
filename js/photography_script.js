f847b96fb88b5bd53de4f9283fc278b1ca1dbf15
ylingene <ylingene@gmail.com>
2016-12-21 15:38:45 -0500

    removed padding on phtography page for mobile devices

--- js/photography_script.js
+++ js/photography_script.js
@@ -68,32 +68,3 @@ $(".fancybox").attr('rel', 'gallery').fancybox({
     }
 });
 
-
-$(document).ready( function() {
-
-  // show nav bar at a certain breakpoint depending on the window size (corresponds to the hero-div size)
-  $(window).scroll(function(){                          
-      if ($(window).width() <= 768) {
-          if ($(this).scrollTop() >= $(window).height() * .75) {
-            $('.hidden-nav').stop().fadeIn(300);
-          // alert('less than or equal to 768')
-          }
-          else {
-          $('.hidden-nav').stop().fadeOut(275);
-          }
-      } 
-      else if ($(window).width() > 768) {
-         if ($(this).scrollTop() >= $(window).height()) {
-          $('.hidden-nav').stop().fadeIn(300);
-        }
-        else {
-          $('.hidden-nav').stop().fadeOut(275);
-        }
-      }
-      else {
-          $('.hidden-nav').stop().fadeOut(275);
-      }
-  });
-  
-});
-
