d17b4f1bde39cfb4a9a3615e1e6402349428570d
ylingene <ylingene@gmail.com>
2016-12-21 22:03:20 -0500

    finished updating navbar

--- js/home_script.js
+++ js/home_script.js
@@ -1,25 +1,9 @@
-// show nav bar at a certain breakpoint depending on the window size (corresponds to the hero-div size)
-$(window).scroll(function(){                          
-    if ($(window).width() <= 768) {
-        if ($(this).scrollTop() >= $(window).height() * .75) {
-            // $('.navbar').addClass('hidden-nav');
-            // $('.hidden-nav').stop().fadeIn(300);
-            // alert('less than or equal to 768')
-        }
-        else {
-            // $('.hidden-nav').stop().fadeOut(275);
-        }
-    } 
-    else if ($(window).width() > 768) {
-        if ($(this).scrollTop() >= $(window).height()) {
-            console.log('hello')
-            $('.navbar').animate({backgroundColor: '#292c31'}, 100);
-        }
-        else {
-            $('.navbar').animate({backgroundColor: 'transparent'}, 100);
-        }
+// show nav bar at a certain breakpoint
+$(window).scroll(function(){                           
+    if ($(this).scrollTop() >= $(window).height()) {
+        $('.navbar-fixed-top').addClass('navbar-background');
     }
     else {
-        $('.hidden-nav').stop().fadeOut(275);
+        $('.navbar-fixed-top').removeClass('navbar-background');
     }
 });
