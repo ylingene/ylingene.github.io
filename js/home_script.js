77fbca5d270abe73e9cc3ab0168b180c599de462
ylingene <ylingene@gmail.com>
2016-12-21 19:19:25 -0500

    fixed navbar on home page

--- js/home_script.js
+++ js/home_script.js
@@ -1,17 +1,45 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    $(window).scroll(function(){                          
-            if ($(this).scrollTop() >= $(window).height()) {
-                $('#menu').stop().fadeIn(300);
-            } else {
-                $('#menu').stop().fadeOut(275);
-            }
-        });
+//     $(window).scroll(function(){                          
+//             if ($(this).scrollTop() >= $(window).height()) {
+//                 $('#menu').stop().fadeIn(300);
+//             } else {
+//                 $('#menu').stop().fadeOut(275);
+//             }
+//         });
 
-//     $('.overlay').hover(function() {
-//     $(this).find('img').fadeTo(500, 0.5);
-// }, function() {
-//     $(this).find('img').fadeTo(500, 1);
-// });
+// //     $('.overlay').hover(function() {
+// //     $(this).find('img').fadeTo(500, 0.5);
+// // }, function() {
+// //     $(this).find('img').fadeTo(500, 1);
+// // });
     
+// });
+
+$(document).ready( function() {
+
+  // show nav bar at a certain breakpoint depending on the window size (corresponds to the hero-div size)
+  $(window).scroll(function(){                          
+      if ($(window).width() <= 768) {
+          if ($(this).scrollTop() >= $(window).height() * .75) {
+            $('.hidden-nav').stop().fadeIn(300);
+          // alert('less than or equal to 768')
+          }
+          else {
+          $('.hidden-nav').stop().fadeOut(275);
+          }
+      } 
+      else if ($(window).width() > 768) {
+         if ($(this).scrollTop() >= $(window).height()) {
+          $('.hidden-nav').stop().fadeIn(300);
+        }
+        else {
+          $('.hidden-nav').stop().fadeOut(275);
+        }
+      }
+      else {
+          $('.hidden-nav').stop().fadeOut(275);
+      }
+  });
+  
 });
