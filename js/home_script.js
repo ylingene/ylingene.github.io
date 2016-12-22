de3f7d361b2386c3b5626671415513d4a07281aa
ylingene <ylingene@gmail.com>
2016-12-21 21:37:54 -0500

    testing new home nav bar

--- js/home_script.js
+++ js/home_script.js
@@ -1,45 +1,25 @@
-// $(document).ready(function() {
-
-//     $(window).scroll(function(){                          
-//             if ($(this).scrollTop() >= $(window).height()) {
-//                 $('#menu').stop().fadeIn(300);
-//             } else {
-//                 $('#menu').stop().fadeOut(275);
-//             }
-//         });
-
-// //     $('.overlay').hover(function() {
-// //     $(this).find('img').fadeTo(500, 0.5);
-// // }, function() {
-// //     $(this).find('img').fadeTo(500, 1);
-// // });
-    
-// });
-
-$(document).ready( function() {
-
-  // show nav bar at a certain breakpoint depending on the window size (corresponds to the hero-div size)
-  $(window).scroll(function(){                          
-      if ($(window).width() <= 768) {
-          if ($(this).scrollTop() >= $(window).height() * .75) {
-            $('.hidden-nav').stop().fadeIn(300);
-          // alert('less than or equal to 768')
-          }
-          else {
-          $('.hidden-nav').stop().fadeOut(275);
-          }
-      } 
-      else if ($(window).width() > 768) {
-         if ($(this).scrollTop() >= $(window).height()) {
-          $('.hidden-nav').stop().fadeIn(300);
+// show nav bar at a certain breakpoint depending on the window size (corresponds to the hero-div size)
+$(window).scroll(function(){                          
+    if ($(window).width() <= 768) {
+        if ($(this).scrollTop() >= $(window).height() * .75) {
+            // $('.navbar').addClass('hidden-nav');
+            // $('.hidden-nav').stop().fadeIn(300);
+            // alert('less than or equal to 768')
         }
         else {
-          $('.hidden-nav').stop().fadeOut(275);
+            // $('.hidden-nav').stop().fadeOut(275);
         }
-      }
-      else {
-          $('.hidden-nav').stop().fadeOut(275);
-      }
-  });
-  
+    } 
+    else if ($(window).width() > 768) {
+        if ($(this).scrollTop() >= $(window).height()) {
+            console.log('hello')
+            $('.navbar').animate({backgroundColor: '#292c31'}, 100);
+        }
+        else {
+            $('.navbar').animate({backgroundColor: 'transparent'}, 100);
+        }
+    }
+    else {
+        $('.hidden-nav').stop().fadeOut(275);
+    }
 });
