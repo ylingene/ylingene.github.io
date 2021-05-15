f01c969fc116fca5e937b6bd9eef61366e3d41a9
ylingene <ylingene@gmail.com>
2021-05-14 23:45:17 -0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,38 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
+import Container from "../components/container/container"
 import Seo from "../components/seo"
+import { HOME_PATH } from "../utils/defs"
 
 const NotFoundPage = ({ data, location }) => {
-    // const siteTitle = data.site.siteMetadata.title
-
+    console.log(HOME_PATH)
     return (
-        <div>
-            <Seo title="404: Not Found" />
-            <h1>Not Found</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        </div>
+        <Container>
+            <Seo />
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <div>
+                    <div style={{ maxWidth: 800 }}>
+                        <StaticImage
+                            src="../../content/images/photography/japan-2018/arashiyama.jpg"
+                            alt="Arashiyama"
+                        />
+                    </div>
+                    <h1>Not Found</h1>
+                    <p>
+                        This page doesn't exist. Please head back{" "}
+                        <Link to={HOME_PATH}>here</Link>.
+                    </p>
+                </div>
+            </div>
+        </Container>
     )
 }
 
